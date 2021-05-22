587cefe2fa5087700db3d9b2e560a17b4d3c02e7
Justin Harder <justinharder@t-online.de>
2021-05-22 13:44:45 +0200

    Korrigiere zur Analyse

--- src/main/java/de/trainharder/authentication/domain/model/Main.java
+++ src/main/java/de/trainharder/authentication/domain/model/Main.java
@@ -1,5 +1,9 @@
 package de.trainharder.authentication.domain.model;
 
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class Main
 {
 	public static int test()
